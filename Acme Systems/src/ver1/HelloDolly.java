fa075b92fcac7f9c617ab068704d20f815c35ad6
Joseph Campbell <josephcampbell926@gmail.com>
2022-06-24 13:41:00 -0400

    Returning greeting

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,6 +3,6 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Dolly");
+		System.out.println("Hello Dolly from Eclipse Again");
 	}
 }
